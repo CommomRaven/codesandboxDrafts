18aaf0c0509cdb0e75fffd1ecdef9e9499d5fd1d
Rafael_Germano_90 <rafael.grocha@al.infnet.edu.br>
2026-04-10 17:07:04 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-let frase =
-  "toda ciencia seria superflua se houvesse coincidencia imediata entre a aparencia e a essencia das coisas";
-
-let resultado = frase.replace(/[aeiou]/gi, "");
-
-console.log(resultado);

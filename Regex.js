413dd5eb064413808fd84a6d9388462bd40af15f
Rafael_Germano_90 <rafael.grocha@al.infnet.edu.br>
2026-04-10 17:13:21 -0300

    Regex
    Este é um exemplo clássico de manipulação de strings em JavaScript, utilizando Expressões Regulares (Regex) / This is a classic example of string manipulation in JavaScript, using Regular Expressions (Regex).

---
+++ Regex.js
@@ -0,0 +1,6 @@
+let frase =
+  "toda ciencia seria superflua se houvesse coincidencia imediata entre a aparencia e a essencia das coisas";
+
+let resultado = frase.replace(/[aeiou]/gi, "");
+
+console.log(resultado);

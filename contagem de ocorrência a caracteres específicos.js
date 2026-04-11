8b03ce9d58dbda2c24b1f4521582d7f174773ded
Rafael_Germano_90 <rafael.grocha@al.infnet.edu.br>
2026-04-11 18:08:01 -0300

    contagem de ocorrência a caracteres específicos

---
+++ contagem de ocorrência a caracteres específicos.js
@@ -0,0 +1,6 @@
+let frase =
+  "toda ciência seria supérflua se houvesse coincidência imediata entre a aparência e a essência das coisas";
+
+let quantidade = frase.split("a").length - 1;
+
+console.log(quantidade);
